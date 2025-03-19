51039d6600e538d561a51fea9490daff215e48e0
trx714 <151689815+trx714@users.noreply.github.com>
2025-03-19 00:35:19 +0000

    codigo finalizado

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,179 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main(){
+// variaveis do jogo
+    int rainha = 0, bispo = 0, torre = 0, rainha_mov, bispo_mov, torre_mov;
+    int escolha, escolhadepeca;
+// inicialização do jogo
+    printf("jogo de xadrez\n");
+    printf("1. Iniciar jogo\n");
+    printf("2. Ver as regras\n");
+    printf("3. Sair do jogo\n");
+    scanf("%d", &escolha);
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+// escolha de uma das 3 peças
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    switch (escolha)
+    {
+    case 1:
+    printf("Jogo iniciado, escolha a peça para jogar\n");
+    printf("1. Rainha\n");
+    printf("2. Bispo\n");
+    printf("3. Torre\n");
+    scanf("%d", &escolhadepeca);
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    switch (escolhadepeca)
+    {// escolha da direção da rainha
+    case 1:
+    printf("Você escolheu a Rainha, escolha a direção que você deseja ir\n");
+    printf("1. Cima\n");
+    printf("2. Direita\n");
+    printf("3. Esquerda\n");
+    printf("4. Baixo\n");
+    printf("5. Cima direita\n");
+    printf("6. Cima esquerda\n");
+    printf("7. Baixo direita\n");
+    printf("8. Baixo esquerda\n");
+    scanf("%d", &rainha_mov);
+// movimento da rainha com (for)
+    if (rainha_mov == 1){
+        for ( rainha; rainha <= 8; rainha++)
+        {
+            printf("Cima\n");
+        }
+        
+    } else if (rainha_mov == 2){
+        for ( rainha; rainha <= 8; rainha++)
+        {
+            printf("Direita\n");
+        }
+    } else if (rainha_mov == 3){
+        for ( rainha; rainha <= 8; rainha++)
+        {
+            printf("Esquerda\n");
+        }
+    } else if (rainha_mov == 4){
+        for ( rainha; rainha <= 8; rainha++)
+        {
+            printf("Baixo\n");
+        }
+    } else if (rainha_mov == 5){
+        for ( rainha; rainha <= 8; rainha++)
+        {
+            printf("Cima direita\n");
+        }
+    } else if (rainha_mov == 6){
+        for ( rainha; rainha <= 8; rainha++)
+        {
+            printf("Cima Esquerda\n");
+        }
+    } else if (rainha_mov == 7){
+        for ( rainha; rainha <= 8; rainha++)
+        {
+            printf("Baixo direita\n");
+        }
+    }else {
+        for ( rainha; rainha <= 8; rainha++)
+        {
+            printf("Baixo esquerda\n");
+        }
+    }
+        break;
+// escolha da direção do bispo
+    case 2:
+    printf("Você escolheu o Bispo, escolha a direção que você deseja ir\n");
+    printf("1. Cima direita\n");
+    printf("2. Cima esquerda\n");
+    printf("3. Baixo direita\n");
+    printf("4. Baixo esquerda\n");
+    scanf("%d", &bispo_mov);
+// movimento do bispo com (while)
+    if (bispo_mov == 1){
+        while (bispo <= 5)
+        {
+            printf("Cima direita\n");
+            bispo++;
+        }
+        
+    } else if (bispo_mov == 2){
+        while (bispo <= 5)
+        {
+            printf("Cima esquerda\n");
+            bispo++;
+        }
+    } else if (bispo_mov == 3){
+        while (bispo <= 5)
+        {
+            printf("Baixo direita\n");
+            bispo++;
+        }
+    } else {
+        while (bispo <= 5)
+        {
+            printf("Baixo esquerda\n");
+            bispo++;
+        }
+    }
+        break;
+// escolha da direção da torre
+    case 3:
+    printf("Você escolheu o Bispo, escolha a direção que você deseja ir\n");
+    printf("1. Cima\n");
+    printf("2. Direita\n");
+    printf("3. Esquerda\n");
+    printf("4. Baixo\n");
+    scanf("%d", &torre_mov);
+// movimento da torre com (do-while)
+    if (torre_mov == 1){
+        do
+        {
+            printf("Cima\n");
+            torre++;
+        } while (torre <= 5);
+        
+    } else if (torre_mov == 2){
+        do
+        {
+            printf("Direita\n");
+            torre++;
+        } while (torre <= 5);
+        
+    } else if (torre_mov == 3){
+        do
+        {
+            printf("Esquerda\n");
+            torre++;
+        } while (torre <= 5);
+        
+    } else {
+        do
+        {
+            printf("Baixo\n");
+            torre++;
+        } while (torre <= 5);
+    }
+        break;
+    
+    default:
+    printf("opção invalida!\n");
+        break;
+    }
+        break;
+    
+case 2: // opção da inicialização do jogo
+    printf("Regras Do Jogo\n");
+    printf("Você deve escolher uma das peças e depois escolher qual a direção que ela deve ir\n");
+        
+        break;
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    case 3: // opção da inicialização do jogo
+    printf("Você saiu do jogo\n");
+        break;
+    
+    default:
+    printf("opção invalida!\n");
+        break;
+    }
 
     return 0;
 }
